9e93cb20c8b0b6e5c36afe5b46b61680def98914
David Hale and Dawson Mortenson <david@>
2017-08-22 09:41:18 -0700

    add movie objects

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+// Backend
+var starWars = {name: "Star Wars VIII", times: ["12:00pm", "1:00pm", "4:00pm", "6:00pm", "9:00pm", "11:00pm"]};
+var it = {name: "It", times: ["2:00pm", "8:00pm", "9:00pm", "11:00pm", "12:00am"]};
+var citizenKane = {name: "Citizen Kane", times: ["11:00am", "1:00pm", "3:00pm"]};
